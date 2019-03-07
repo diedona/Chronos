3623bd19ea73fc765d10adecee92c16cbaf1b509
diego.dona <diego.dona@outlook.com.br>
2019-03-07 10:11:37 -0300

    routerLink

--- chronos-frontend/src/app/login/login.module.ts
+++ chronos-frontend/src/app/login/login.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { MaterialImportsModule } from '../shared/material-imports/material-imports.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MaterialImportsModule
   ]
 })
 export class LoginModule { }
