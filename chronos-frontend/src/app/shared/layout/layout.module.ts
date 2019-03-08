08627909022c2cd72247af59729e64740c908229
diego.dona <diego.dona@outlook.com.br>
2019-03-08 10:32:30 -0300

    mudanças no sistema de arquivos do sass add !important pra sobrescrever classes do material

--- chronos-frontend/src/app/shared/layout/layout.module.ts
+++ chronos-frontend/src/app/shared/layout/layout.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
 import { RouterModule } from '@angular/router';
+import { BaseModule } from '../base/base.module';
 
 @NgModule({
   declarations: [AppLayoutComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BaseModule
   ]
 })
 export class LayoutModule { }
