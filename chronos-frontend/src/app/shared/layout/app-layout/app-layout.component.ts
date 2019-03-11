9f4e706e3ddc3a877c79fba1a9db21f8cf44574e
diego.dona <diego.dona@outlook.com.br>
2019-03-11 09:26:13 -0300

    divisão em componentes

--- chronos-frontend/src/app/shared/layout/app-layout/app-layout.component.ts
+++ chronos-frontend/src/app/shared/layout/app-layout/app-layout.component.ts
@@ -7,12 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppLayoutComponent implements OnInit {
 
-  currentDate: number;
-
   constructor() { }
 
   ngOnInit() {
-    this.currentDate = Date.now();
   }
 
 }
--- chronos-frontend/src/app/shared/layout/layout.module.ts
+++ chronos-frontend/src/app/shared/layout/layout.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
 import { RouterModule } from '@angular/router';
 import { BaseModule } from '../base/base.module';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
-  declarations: [AppLayoutComponent],
+  declarations: [AppLayoutComponent, AppHeaderComponent, AppFooterComponent],
   imports: [
     CommonModule,
     RouterModule,
