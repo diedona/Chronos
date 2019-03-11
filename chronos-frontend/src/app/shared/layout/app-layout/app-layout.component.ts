e87f16e8d7a503767486555e96ccff33eaec6ead
diego.dona <diego.dona@outlook.com.br>
2019-03-11 09:10:00 -0300

    footer

--- chronos-frontend/src/app/shared/layout/app-layout/app-layout.component.ts
+++ chronos-frontend/src/app/shared/layout/app-layout/app-layout.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppLayoutComponent implements OnInit {
 
+  currentDate: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.currentDate = Date.now();
   }
 
 }
