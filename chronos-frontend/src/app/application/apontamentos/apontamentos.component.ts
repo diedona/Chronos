8f9e1ffdc437796d6722196f0f190115418b63ea
diego.dona <diego.dona@outlook.com.br>
2019-03-11 09:34:54 -0300

    rotas stubs

---
+++ chronos-frontend/src/app/application/apontamentos/apontamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apontamentos',
+  templateUrl: './apontamentos.component.html',
+  styleUrls: ['./apontamentos.component.scss']
+})
+export class ApontamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
