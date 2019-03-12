6368c2455108f2db3efc45f5dbfc844d48fa4ef6
diego.dona <diego.dona@outlook.com.br>
2019-03-12 11:21:19 -0300

    app-form-group comunicação entre componentes pai/filho para exibir validação na ui

--- chronos-frontend/src/app/shared/components/custom-components.module.ts
+++ chronos-frontend/src/app/shared/components/custom-components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorHintComponent } from './error-hint/error-hint.component';
+import { FormGroupComponent } from './form-group/form-group.component';
 
 @NgModule({
-  declarations: [ErrorHintComponent],
+  declarations: [
+    ErrorHintComponent, FormGroupComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [ErrorHintComponent]
+  exports: [
+    ErrorHintComponent, FormGroupComponent
+  ]
 })
 export class CustomComponentsModule { }
--- chronos-frontend/src/app/shared/components/error-hint/error-hint.component.ts
+++ chronos-frontend/src/app/shared/components/error-hint/error-hint.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl, NgForm } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,7 @@ export class ErrorHintComponent implements OnInit {
 
   @Input() refForm: NgForm;
   @Input() refFormControl: FormControl;
+  @Output() emitterErrorState: EventEmitter<boolean> = new EventEmitter();
 
   constructor(
 
@@ -26,6 +27,7 @@ export class ErrorHintComponent implements OnInit {
         (this.refFormControl.dirty || this.refFormControl.touched || this.refForm.submitted)
       );
 
+    this.emitterErrorState.emit(isInError);
     return isInError;
   }
 
